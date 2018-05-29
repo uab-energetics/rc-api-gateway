f671156717c126edfead49b57f8061a7af76b505
Chris Rocco <chris.rocco7@gmail.com>
2018-05-29 18:36:27 -0500

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import httpProxy from 'express-http-proxy'
 import dotenv from 'dotenv'
 
+const cors = require('cors')
+
 /* LOAD ENVIRONMENT VALUES */
 dotenv.config({ path: ".env" });
 
@@ -9,12 +11,18 @@ dotenv.config({ path: ".env" });
 const app = express()
 app.set('port', process.env.PORT || 6000)
 
+app.use(cors())
+
 app.post('/auth/login', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
     proxyReqPathResolver: (req) => '/login'
 }))
 
 app.post('/auth/register', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
     proxyReqPathResolver: (req) => '/register'
+}))
+
+app.post('/auth/github', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
+    proxyReqPathResolver: (req) => '/oauth/github'
 }))
 
 // proxy every request for now. *NOTE - future services will be added here as they are developed.
