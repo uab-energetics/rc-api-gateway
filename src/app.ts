5f2f77b083089bb4745e1cbc76ebbf8db8f8c9dc
Chris Rocco <chris.rocco7@gmail.com>
2019-07-27 22:01:53 -0500

    Removed authentication routes from API gateway.

--- src/app.ts
+++ src/app.ts
@@ -14,20 +14,6 @@ app.set('port', process.env.PORT || 6000)
 
 app.use(cors())
 
-app.post('/auth/login', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
-    proxyReqPathResolver: (req) => '/login'
-}))
-
-app.post('/auth/register', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
-    proxyReqPathResolver: (req) => '/register'
-}))
-
-app.get('/auth/github', httpProxy(process.env.RC_AUTHENTICATION_HOST, {
-    proxyReqPathResolver: (req) => '/oauth/github'
-}))
-
-app.get('/oauth/github/callback', httpProxy(process.env.RC_AUTHENTICATION_HOST))
-
 app.use('/projects/*/pub-repos*', httpProxy(process.env.RC_PUBLICATIONS_HOST, {
     proxyReqPathResolver: req => url.parse(req.originalUrl).path
 }))
